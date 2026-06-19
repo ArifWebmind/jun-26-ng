69ae2411db61f5a682602a3ec65ce3956437dc82
Sumit Khandelwal <synergy2411@outlook.com>
2026-06-19 13:11:00 +0530

    presentation added

--- second-app/src/app/layout/main-navigation/main-navigation.component.ts
+++ second-app/src/app/layout/main-navigation/main-navigation.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,5 +12,16 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   templateUrl: './main-navigation.component.html',
   styleUrl: './main-navigation.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainNavigationComponent {}
+export class MainNavigationComponent {
+  private cdRef = inject(ChangeDetectorRef);
+
+  onStop() {
+    this.cdRef.detach();
+    // Angular will not run Digest Cycle for the code written here
+    this.cdRef.reattach();
+    this.cdRef.detectChanges(); // Manually run digest cycle
+    this.cdRef.markForCheck(); // onPush ChangeDetection
+  }
+}
